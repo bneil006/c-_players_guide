8de35deae8468391f79ef9e523afdce2fb6165ee
bneil006 <brmneil2@gmail.com>
2025-02-22 00:24:14 -0600

    some progress on structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+// System is the namespace that the Console class lives in, WriteLine is a method that belongs to the Console Class
+System.Console.WriteLine("Using Writeline"); // BCL (Base Class Library) > System (Namespace in BCL Library) > Console (Class of Namespace System) > WriteLine (Method of Console)
+
+// must tell C# what type of variable we want before variable assignment
+string name = "Brandon"; // This is a string literal
+Console.WriteLine("Hello " + name);
+
+// Console Application > Program > Main > newVariable
+string newVariable = "New String Literal";
+string userName; // we can initialize a varaible without assigning a value to it now
+
+Console.WriteLine("What is your name: ");
+userName = Console.ReadLine();
+Console.WriteLine("Hello, " + userName);
