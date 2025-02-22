b9e42d724c6ed9763e0f7a508095c59c82add482
bneil006 <brmneil2@gmail.com>
2025-02-22 14:58:44 -0600

    simple console application to add and show triangleAreas

--- Program.cs
+++ Program.cs
@@ -1,58 +1,74 @@
 ﻿/*
-The Method / Function below could be a static void if we didn't want to return anything from it, but since we do and since it is a float
-that we want to return, we will call this a static float and return similar to python with a return statment.
-Super simple feeling atleast currently.
+    Starting again from scratch and creating the same method to give the farmer a way to calculate the area of all his triangles.
+    Wanted to do this to put to test my C# knowledge that I gained lastnight.
 */
 
-/*
-Also got a little tired of waiting and moved onto dive a little further into the arrays - I was looking into Lists when i realized that in C# and Python they are different
-an Array and a List in python seem like the same thing, and Array was what I was essentially looking for here (I think)
- */
+List<(float, float)> farmerTriangles = new List<(float, float)> {  }; // all the farmers Triangles (Base (b), Height (h))
 
-static float TriangleFarmerSingle(int b, int h)
+static List<float> triangleAreas(List<(float, float)> triangles)
 {
-    return (b * h) / 2f;
-}
-
-TriangleFarmerSingle(5, 10); // received out result back but haven't yet printed it or assigned it to a variable so its basically doing nothing here.
-
-// this is all feeling super similar to python, I guess learning the fundementals of one language does have some seriously good translation effects on other languages.
-float firstSingleTriangle = TriangleFarmerSingle(5, 10);
-Console.WriteLine($"using the variable assigned to the value of TriangleFarmerSingle(5, 10) (firstSingleTriangle): {firstSingleTriangle}");
-Console.WriteLine($"using the Method directly in the Console.WriteLine(TriangleFarmerSingle(7, 12): {TriangleFarmerSingle(7, 12)}");
-
-/* 
-lets help our farmer out a little more, the C# players guide asks us to create a way for him to calculate the area of a single triangle
-but the poor farmer just told us he has many triangles on their farm. I don't want them to have to do this one at a time, he could lose track.
-that would be sad. I'll just go ahead and figure out how to return an floats[] array of areas for him, he just needs to give me a list of tuples with
-the base (b) and the height (h) and I'll return that array with the areas.
-*/
-
-// kinda hate the way you have to declare and then assign lists, seems extreme
-List<(int, int)> farmersAssets = new List<(int, int)> { (5, 10), (7, 12), (11, 9) };
-static List<float> TriangleFarmerMultiple(List<(int, int)> triangleList)
-{
-    List<float> areas = new List<float>();
-    for (int i = 0; i < triangleList.Count; i++)
+    List<float> result = new List<float> { };
+    foreach ((float, float) item in triangles)
     {
-        var item = triangleList[i];
-        var b = item.Item1;
-        var h = item.Item2;
-        areas.Add(TriangleFarmerSingle(b, h));
+        result.Add((item.Item1 * item.Item2) / 2); // triangle area formula (base * height) / 2
     }
-    return areas;
+    return result;
 }
-List<float> farmersAreas = TriangleFarmerMultiple(farmersAssets);
 
-// foreach loop on all float items in the List<float>
-foreach (float area in farmersAreas)
+var farmerAssets = triangleAreas(farmerTriangles);
+
+
+bool addToList = true;
+while (addToList == true)
 {
-    Console.WriteLine($"FOR EACH LOOP: {area}");
-}
-Console.WriteLine(string.Join(", ", farmersAssets)); // placing all the areas in a single line / Joining them together
+    Console.WriteLine("Would you like to add anymore items to your triangle List? [YES / NO]");
+    string response = Console.ReadLine();
 
-List<(float, float, int)> randomItems = new List<(float, float, int)> { }; // want to learn how to do a foreach on this one too
-randomItems.Add((6.3f, 7.43f, 10));
+    if (response == "yes" || response == "Yes" || response == "YES")
+    {
+        Console.WriteLine("What is the new items base? ");
+        string newBase = Console.ReadLine();
+        int newerBase = Convert.ToInt32(newBase); // i feel like there has to be a better way to convert string to int on user inputs from the ReadLine Method.
+
+        Console.WriteLine("What is the new items height? ");
+        string newHeight = Console.ReadLine();
+        int newerHeight = Convert.ToInt32(newHeight);
+
+        farmerTriangles.Add((newerBase, newerHeight));
+
+        int newArea = (newerBase * newerHeight) / 2;
+        farmerAssets.Add(newArea);
+    }
+    else
+    {
+        addToList = false;
+    }
+}
+
+bool checkLists = true;
+while (checkLists == true)
+{
+    Console.WriteLine("If you would like to see all the Base, Height pairs in your farmerTriangles or all areas in farmerAssets variable TYPE FT OR FA or Quit with any other entry");
+    string userSecondResponse = Console.ReadLine();
+    if (userSecondResponse == "FT")
+    {
+        foreach ((float, float) item in farmerTriangles)
+        {
+            Console.WriteLine($"BASE: {item.Item1}, HEIGHT: {item.Item2}");
+        }
+    }
+    else if (userSecondResponse == "FA")
+    {
+        foreach (float item in farmerAssets)
+        {
+            Console.WriteLine($"AREA: {item}");
+        }
+    }
+    else
+    {
+        checkLists = false;
+    }
+}
 
 
 
