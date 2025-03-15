8d75cdb24ded378c2a853d10eaffbed7c6d8a079
bneil006 <brmneil2@gmail.com>
2025-03-15 00:37:09 -0500

    challenge on tuples page 143

--- Classes/Challenges.cs
+++ Classes/Challenges.cs
@@ -410,13 +410,84 @@ namespace Challenges // Challenges from C# Players Guide
             }
         }
 
-        public static (string, int, int) AddScore(string name, int score, int level)
+        public static void FountainOfObjects() // challenge on page 143
         {
-            return (name, score, level);
+            SpiceType spiceSelection;
+            IngredientType ingredientSelection;
+            FoodType foodSelection;
+
+            Console.WriteLine("MENU");
+            Console.WriteLine("--------------------");
+            Console.WriteLine($"SPICES: {SpiceType.Spicy}, {SpiceType.Salty}, {SpiceType.Sweet}");
+            Console.WriteLine($"INGREDIENT: {IngredientType.Mushroom}, {IngredientType.Carrot}, {IngredientType.Potatoe}, {IngredientType.Chicken}");
+            Console.WriteLine($"FOOD: {FoodType.Soup}, {FoodType.Stew}, {FoodType.Gumbo}");
+            Console.WriteLine("--------------------");
+
+            string userSelection = userChoice("SPICE");
+            spiceSelection = userSelection switch
+            {
+                "SPICY" => SpiceType.Spicy,
+                "SALTY" => SpiceType.Salty,
+                "SWEET" => SpiceType.Sweet,
+                _ => SpiceType.Spicy
+            };
+
+            userSelection = userChoice("INGREDIENT");
+            ingredientSelection = userSelection switch
+            {
+                "MUSHROOM" => IngredientType.Mushroom,
+                "CARROT" => IngredientType.Carrot,
+                "POTATOE" => IngredientType.Potatoe,
+                "CHICKEN" => IngredientType.Chicken,
+                _ => IngredientType.Mushroom
+            };
+
+            userSelection = userChoice("FOOD");
+            foodSelection = userSelection switch
+            {
+                "SOUP" => FoodType.Soup,
+                "STEW" => FoodType.Stew,
+                "GUMBO" => FoodType.Gumbo,
+                _ => FoodType.Soup
+            };
+
+            string userChoice(string type)
+            {
+                string userSelection;
+                Console.WriteLine("*** If you don't choose an item from the menu, we will choose for you ***");
+                Console.WriteLine("Choose a Spice, Ingredient, Food");
+                Console.Write($"{type}: ");
+                userSelection = Console.ReadLine().ToUpper();
+                Console.WriteLine("--------------------");
+                return userSelection;
+            }
+
+            Console.WriteLine($"{spiceSelection} {ingredientSelection} {foodSelection}");
         }
-        
+
 
     }
+    enum SpiceType
+    {
+        Spicy,
+        Salty,
+        Sweet
+    }
+    enum IngredientType
+    {
+        Mushroom,
+        Chicken,
+        Carrot,
+        Potatoe
+    }
+
+    enum FoodType
+    {
+        Soup,
+        Stew,
+        Gumbo
+    }
+
 
     enum ChestStates // challenge on page 135 SimulaChest()
     {
--- Program.cs
+++ Program.cs
@@ -1,50 +1,7 @@
 ﻿using System;
 using Challenges;
 
-List<(string, int, int)> highScores = new List<(string, int, int)>();
 
-highScores.Add(OC.AddScore("Luna", 89, 8));
-highScores.Add(OC.AddScore("Haley", 150, 15));
-highScores.Add(OC.AddScore("Brandon", 5, 2));
-highScores.Add(OC.AddScore("Sadie", 870, 25));
-
-Console.WriteLine("UNSORTED LIST");
-foreach (var score in highScores)
-{
-    Console.WriteLine($"Name: {score.Item1}, Score: {score.Item2}, Level: {score.Item3}");
-}
-Console.WriteLine("__________________________________________");
-Console.WriteLine();
-
-List<(string, int, int)> DisplayHighScores(List<(string, int, int)> scores)
-{
-    List<(string, int, int)> scoreList = new List<(string, int, int)>();
-
-    while (scores.Count > 0)
-    {
-        (string, int, int) newTuple = ("default", 1, 1);
-        int highestScore = int.MinValue;
-        foreach (var score in scores)
-        {
-            if (score.Item2 > highestScore)
-            {
-                highestScore = score.Item2;
-                newTuple = (score.Item1, score.Item2, score.Item3);
-            }
-        }
-        scoreList.Add(newTuple);
-        scores.Remove(newTuple);
-    }
-    return scoreList;
-}
-
-Console.WriteLine("SORTED LIST");
-var newSortedList = DisplayHighScores(highScores);
-foreach (var score in newSortedList)
-{
-    Console.WriteLine($"Name: {score.Item1}, Score: {score.Item2}, Level: {score.Item3}");
-}
-Console.WriteLine("__________________________________________");
-Console.WriteLine();
+OC.FountainOfObjects();
 
 Console.ReadLine();
