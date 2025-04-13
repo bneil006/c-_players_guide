a60b917eb3698f20439459e7df47d47a40837931
bneil006 <brmneil2@gmail.com>
2025-04-13 15:40:55 -0500

    started working with Delegates but after a little bit switched over to Action and Func for some more flexiability

--- Program.cs
+++ Program.cs
@@ -1,46 +1,10 @@
-﻿using Extensions;
+﻿
+Action<int> PrintInt = value => Console.WriteLine(value);
+Func<int, int, int> AddItems = (a, b) => a + b;
 
-Random random = new Random();
-CoinFlip coinFlipFair = new CoinFlip();
-CoinFlip coinFlipUnfair = new CoinFlip();
-
-Dictionary<string, int> evenOdds = new Dictionary<string, int>();
-Dictionary<string, int> unevenOdds = new Dictionary<string, int>();
-
-for (int i = 0; i < 1000; i++)
+static void MyNewMethod(Action<int> log, Func<int, int, int> operation, int a, int b)
 {
-    string side = coinFlipFair.CoinFlipSpecial(false);
-    if (evenOdds.ContainsKey(side))
-    {
-        evenOdds[side] += 1;
-    }
-    else
-    {
-        evenOdds[side] = 1;
-    }
+    log(operation(a, b));
 }
 
-for (int i = 0; i < 1000; i++)
-{
-    string side = coinFlipUnfair.CoinFlipSpecial(true);
-    if (unevenOdds.ContainsKey(side))
-    {
-        unevenOdds[side] += 1;
-    }
-    else
-    {
-        unevenOdds[side] = 1;
-    }
-}
-
-foreach (var side in evenOdds)
-{
-    Console.WriteLine(side);
-}
-
-foreach (var side in unevenOdds)
-{
-    Console.WriteLine(side);
-}
-
-Console.ReadLine();
+MyNewMethod(PrintInt, AddItems, 3, 8);
