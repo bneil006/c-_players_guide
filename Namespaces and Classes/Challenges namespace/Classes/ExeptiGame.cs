1f4a6d3a90f7bf945fd3a72cf2bd743fb8634edc
bneil006 <brmneil2@gmail.com>
2025-04-14 14:27:15 -0500

    Completed Exceptions challenge on page 290

---
+++ Namespaces and Classes/Challenges namespace/Classes/ExeptiGame.cs
@@ -0,0 +1,108 @@
+﻿using System.Linq.Expressions;
+
+namespace Challenges
+{
+    public class ExeptiGame // Challenge on Page 290
+    {
+        private bool gameRunning = true;
+
+        private Random random = new Random();
+        private int oatMealCookie;
+        private List<int> pickedNumbers = new List<int>();
+
+        private ExeptiPlayer playerOne;
+        private ExeptiPlayer playerTwo;
+
+        public ExeptiGame(ExeptiPlayer playerOne, ExeptiPlayer playerTwo)
+        {
+            this.playerOne = playerOne;
+            this.playerTwo = playerTwo;
+            oatMealCookie = random.Next(0, 10);
+        }
+
+        public void Game()
+        {
+            Console.WriteLine(
+                "GAME: COOKIE EXCEPTION\n" +
+                "There are 10 Cookies that need to be gathered and eaten. There are 9 Chocolate Chip Cookies and One Oatmeal Cookie.\n" +
+                "Eating a Chocolate chip cookie keeps you in the game.\n" +
+                "Eating the Oatmeal cookie loses you the game.\n" +
+                "GoodLuck!\n");
+
+            while (gameRunning)
+            {
+                gameRunning = Run(playerOne);
+                if (!gameRunning) break;
+
+                gameRunning = Run(playerTwo);
+            }
+        }
+
+        public bool Run(ExeptiPlayer player)
+        {
+            
+            while (true)
+            {
+                int number = player.ChooseNumber();
+
+                if (number == oatMealCookie)
+                {
+                    Console.WriteLine($"{player.name} ATE THE OATMEAL COOKIE. GAMEOVER YOU LOSE.");
+                    return false;
+                }
+
+                if (!pickedNumbers.Contains(number))
+                {
+                    pickedNumbers.Add(number);
+                    Console.WriteLine($"{player.name} ATE A CHOCOLATE CHIP COOKIE. GOOD JOB, YOU'RE STILL IN THIS.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("That number / Cookie was already choosen, choose again.");
+                }
+            }
+
+        }
+
+        public void GetOatMealCookie()
+        {
+            Console.WriteLine(oatMealCookie);
+        }
+
+        public void GetPickedNumbers()
+        {
+            foreach (int number in pickedNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+    }
+
+    public class ExeptiPlayer
+    {
+        public string name;
+        public ExeptiPlayer(string name)
+        {
+            this.name = name;
+        }
+
+        public int ChooseNumber()
+        {
+            int number = -1;
+            while (number < 0 || number > 9)
+            {
+                try
+                {
+                    Console.Write($"{name} Choose a number: ");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if (number < 0 || number > 9) Console.WriteLine("Number must be between 0 and 9\n");
+                }
+                catch (FormatException e) { Console.WriteLine(e.Message); }
+                catch (OverflowException e) { Console.WriteLine(e.Message); }
+            }
+            return number;
+        }
+    }
+}
--- Namespaces and Classes/DND namespace/Dnd Sessions/dnd.cs
+++ Namespaces and Classes/DND namespace/Dnd Sessions/dnd.cs
@@ -1,35 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿////SessionEvents.AddEvent(
+////    location: "",
+////    player: "",
+////    text:
+////    "");
+
+//SessionEvents.GetEvents();
+//Console.WriteLine();
+//SessionEvents.GetEventDetails();
+
+//SessionEvents.WriteEventsToFile("");
 
 namespace DND
 {
     public static class SessionEvents
     {
         private static Dictionary<int, string> sessionEvent = new Dictionary<int, string>();
-        private static Dictionary<int, (string, string, string)> locationPlayer = new Dictionary<int, (string, string, string)>();
+        private static Dictionary<int, (string, string, string)> logDetails = new Dictionary<int, (string, string, string)>();
         public static void AddEvent(string text, string location = "Unknown", string player = "Unknown", string dndEvent = "Unknown")
         {
             int id = sessionEvent.Count + 1;
             sessionEvent.Add(id, text);
-            locationPlayer.Add(id, (dndEvent, player, location));
+            logDetails.Add(id, (dndEvent, player, location));
         }
 
-        public static void RemoveEvent(int id, string text)
+        public static void RemoveEvent(int id)
         {
             if (sessionEvent.ContainsKey(id))
             {
                 sessionEvent.Remove(id);
+                logDetails.Remove(id);
             }
         }
 
-        public static void UpdateEvent(int id, string text)
+        public static void UpdateEvent(int id, string text, string location = "Unknown", string player = "Unknown", string dndEvent = "Unknown")
         {
             if (sessionEvent.ContainsKey(id))
             {
                 sessionEvent[id] = text;
+                logDetails[id] = (dndEvent, location, player);
             }
         }
 
@@ -45,13 +53,37 @@ namespace DND
             }
         }
 
-        public static void GetLocationAndPlayer()
+        public static void GetEventDetails()
         {
-            foreach (var item in locationPlayer)
+            foreach (var item in logDetails)
             {
                 Console.Write(item);
                 Console.WriteLine();
             }
+        }
+
+        public static void WriteEventsToFile(string fileName)
+        {
+            string fullSession = $"";
+            foreach (var item in sessionEvent)
+            {
+                fullSession += 
+                    $"ID:" +
+                    $"{item.Key}\n" +
+                    $"EVENTS:\n" +
+                    $"{item.Value}\n";
+
+            }
+
+            foreach (var item in logDetails)
+            {
+                fullSession +=
+                    $"ID:" +
+                    $"{item.Key}\n" + 
+                    $"DETAILS:\n" +
+                    $"{item.Value}\n";
+            }
+            File.WriteAllText($"C:\\Users\\brmne\\Desktop\\C# Projects\\workspaces\\github.com\\players_guide\\Namespaces and Classes\\Files\\{fileName}.txt", fullSession);
         }
     }
 }
